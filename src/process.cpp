254b5bff1e2ee19df1015980e186b0dee089d5af
abhilashrj <abhi.r.j95@gmail.com>
2021-05-12 00:06:18 -0600

    Added gaussian blurring, countour drawing

--- src/process.cpp
+++ src/process.cpp
@@ -21,7 +21,7 @@
 #include <pcl/search/kdtree.h>
 #include <image_transport/image_transport.h>
 #include <fstream>
-
+#include "std_msgs/String.h"
 #include <vector>
 
 #include <message_filters/subscriber.h>
@@ -39,12 +39,13 @@
 typedef pcl::PointXYZRGB Point;
 typedef sensor_msgs::PointCloud2 PCloud2;
 
-ros::Publisher pub_intermediate;
+ros::Publisher pub_intermediate, ros_image_pub;
 image_transport::Publisher image_pub_;
 
 ros::Subscriber sub; 
 float min_z, max_z, voxel_size,seg_dist_tr, plane_perc;
 int min_cluster_size, max_cluster_size;
+cv::RNG rng(12345);
 
 double point2planedistnace(pcl::PointXYZ pt, pcl::ModelCoefficients::Ptr coefficients){
     double f1 = fabs(coefficients->values[0]*pt.x+coefficients->values[1]*pt.y+coefficients->values[2]*pt.z+coefficients->values[3]);
@@ -276,7 +277,7 @@ void process_cloud(const sensor_msgs::ImageConstPtr& image, const sensor_msgs::C
   cv_bridge::CvImagePtr cv_ptr;
     try
     {
-      cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::RGB8);
+      cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::TYPE_16UC1);
     }
     catch (cv_bridge::Exception& e)
     {
@@ -284,7 +285,10 @@ void process_cloud(const sensor_msgs::ImageConstPtr& image, const sensor_msgs::C
       return;
     }
 
+  cv::Mat canvas = cv::Mat::zeros(width,height,CV_8UC1);
+  cv::Mat median_blur_canvas =  cv::Mat::zeros(width,height,CV_8UC1);
   int j=0;
+  float depth[width][height];
   for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it)
   {
     for (const auto& idx : it->indices){
@@ -321,20 +325,87 @@ void process_cloud(const sensor_msgs::ImageConstPtr& image, const sensor_msgs::C
       }
         // std::cout << pixel_pos_x << pixel_pos_x << "pixel pos";
         cv_ptr->image.at<cv::Vec3b>(pixel_pos_y,pixel_pos_x)[0] = 255; 
-			  cv_ptr->image.at<cv::Vec3b>(pixel_pos_y,pixel_pos_x)[1] = 0;  
-			  cv_ptr->image.at<cv::Vec3b>(pixel_pos_y,pixel_pos_x)[2] = 0;  
+			  cv_ptr->image.at<cv::Vec3b>(pixel_pos_y,pixel_pos_x)[1] = 255;  
+			  cv_ptr->image.at<cv::Vec3b>(pixel_pos_y,pixel_pos_x)[2] = 255;  
+        canvas.at<unsigned char>(pixel_pos_y, pixel_pos_x) = 255;
+        depth[pixel_pos_y][pixel_pos_x] = z;
     }
     j++;
   }
+  // for(int i=0;i<width;i++){
+  //   for(int j=0;j<height;j++){
+  //     std::cout << depth[i][j] << " ";
+  //   }
+  //   std::cout << "\n";
+  // }
 
-   toROSMsg(*cloud_cluster, *intermediate_cloud);
+  toROSMsg(*cloud_cluster, *intermediate_cloud);
   intermediate_cloud->header.frame_id = "kinect2_ir_optical_frame";   
   pub_intermediate.publish(intermediate_cloud);
- 
-
-    image_pub_.publish(cv_ptr->toImageMsg());
   
-  // convert_point_cloud_to_opencvimage(cloud_cluster);
+  cv::GaussianBlur(canvas, median_blur_canvas,  cv::Size(15, 15), 0, 0 );
+ 
+  image_pub_.publish(cv_ptr->toImageMsg());
+  std::vector<std::vector<cv::Point>> contours;
+  std::vector<cv::Vec4i> hierarchy;
+  cv::findContours(median_blur_canvas, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+  std::vector<cv::Moments> mu(contours.size());
+  std::vector<cv::Point2f> mc(contours.size());
+  cv::Mat drawing = cv::Mat::zeros(cv_ptr->image.size(), CV_8UC3);
+  
+  cv_bridge::CvImagePtr depth_ptr;
+    try
+    {
+      depth_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::TYPE_16UC1);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+  
+
+  for(int i = 0; i< contours.size(); i++)
+  {
+    cv::Scalar color = cv::Scalar(rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+    drawContours(drawing, contours, (int)i, color, 2, cv::LINE_8, hierarchy, 0);
+    mu[i] = moments(contours[i], true);
+    mc[i] = cv::Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00);
+    drawing.at<uchar>(int(mu[i].m10/mu[i].m00) , int(mu[i].m01/mu[i].m00)) = 0;
+
+    //Calculating distance between cluster center and image center
+    int ox = int(mu[i].m10/mu[i].m00);
+    int oy = int(mu[i].m01/mu[i].m00);
+    float depth_o = depth_ptr->image.at<float>(ox, oy);
+
+    float depth_c = depth_ptr->image.at<float>(int(centre_x), int(centre_y));
+
+    float vx = (ox - centre_x)*depth_o; 
+    float vy = (oy - centre_y)*depth_o;
+    float vz =  depth_o - depth_c ;
+
+
+    float distance = sqrt(vx*vx + vy*vy + vz*vz);
+    std::cout << "Distance: " << depth_o << " " <<  depth_c << " "  << ox << " "  << oy << " "  << centre_x << " " << centre_y << "\n";
+    7.94645e-34 4.93612e-33                                             39           269               255.141         207.707
+
+  } 
+  
+  
+
+
+
+cv_bridge::CvImage img_bridge;
+sensor_msgs::Image img_msg; 
+std_msgs::Header header; 
+header.stamp = ros::Time::now(); 
+img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, drawing);
+//img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::TYPE_8UC1, median_blur_canvas);
+img_bridge.toImageMsg(img_msg); 
+ros_image_pub.publish(img_msg);
+
+//image_pub_.publish(cv_ptr->toImageMsg());
+//convert_point_cloud_to_opencvimage(cloud_cluster);
 
  
 }
@@ -360,15 +431,17 @@ int main(int argc, char **argv)
   nh.getParam("/kinect_node/min_cluster_size", min_cluster_size);
   nh.getParam("/kinect_node/max_cluster_size", max_cluster_size);
   pub_intermediate = nh.advertise<PCloud2>("/intermediate", 1, true);
+  ros_image_pub = nh.advertise<sensor_msgs::Image>("/countours", 1, true);
   image_transport::ImageTransport it_(nh);
   image_pub_ = it_.advertise("/image_converter/output_video", 1);
 
 
   //sub = nh.subscribe("/kinect2/sd/points", 1, process_cloud);
 
-  message_filters::Subscriber<sensor_msgs::Image> image_sub(nh, "/kinect2/sd/image_color_rect", 1);
+  message_filters::Subscriber<sensor_msgs::Image> image_sub(nh, "/kinect2/sd/image_depth_rect", 1);
   message_filters::Subscriber<sensor_msgs::CameraInfo> info_sub(nh, "/kinect2/sd/camera_info", 1);
   message_filters::Subscriber<sensor_msgs::PointCloud2> pc_sub(nh, "/kinect2/sd/points", 1);
+
   message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::PointCloud2> sync(image_sub, info_sub, pc_sub, 10);
   sync.registerCallback(boost::bind(&process_cloud, _1, _2, _3));
 
